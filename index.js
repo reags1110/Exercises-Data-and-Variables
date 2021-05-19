ef94297b5348b36a228635fed51939be5ef13214
Reags1110 <>
2021-05-19 04:44:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// 1. Declare and assign the variables here:
+let shuttleName = 'Determination';
+let shuttleSpeedMph = '17500';
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
+let milesToMars = 225000000*0.621;
+let hoursToMars = 139725000/17500;
+let daysToMars = 7984.2857142857/24;
+let milesToMoon = 38400;
+let hoursToMoon = 38400/17500;
+let daysToMoon = 2.1942857143/24;
+console.log(shuttleName+" will take " +daysToMoon+ " days to reach the Moon.");
+
+
+
+
+
+
+
+
+// 2. Use console.log to print the 'typeof' each variable. Print one item per line.
+
+
+
+
+// Code your solution to exercises 3 and 4 here:
+
+
+
+
+
+
+
+// Code your solution to exercise 5 here:
